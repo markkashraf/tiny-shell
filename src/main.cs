101c2ab1919715bdfaa9df2a1965b3d7a29d921f
markkashraf <mark2001rko2001@gmail.com>
2024-12-06 12:42:46 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -9,15 +9,38 @@ Console.Write("$ ");
 // Wait for user input
 string? input = Console.ReadLine();
 
+string[] inputArr = input.Split();
 
-Dictionary<string, int> dict = new Dictionary<string, int>();
+Dictionary<string, Command> dict = new Dictionary<string, Command>();
+
+dict["echo"] = Echo;
 
 while (input != "exit 0")
 {
-    if (!dict.ContainsKey(input))
+    if (input!=null && !dict.ContainsKey(inputArr[0]))
     {
         Console.WriteLine($"{input}: not found");
     }
+    else
+    {
+        Console.WriteLine(dict[inputArr[0]](inputArr));
+    }
     Console.Write("$ ");
     input = Console.ReadLine();
+    inputArr = input.Split();
 }
+
+
+
+string Echo(string[] s){
+
+    string result ="";
+    for(int i = 1; i<s.Length; i++) 
+    { 
+        result+= s[i];
+        if(i!=s.Length-1) result+=" ";
+    }
+    return result;
+}
+
+delegate string Command(string[] s);
